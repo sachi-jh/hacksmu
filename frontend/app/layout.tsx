c944c21196879f588f96e9be274d070f7c86b0f0
Caleb Bae <baecal000@gmail.com>
2024-10-05 14:48:13 -0500

    Took out the dark

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
-import { ThemeProvider } from "next-themes";
 import MainNav from "@/components/MainNav";
 
 const geistSans = localFont({
@@ -30,21 +29,14 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <ThemeProvider
-          attribute="class"
-          defaultTheme="dark"
-          enableSystem
-          disableTransitionOnChange
-        >
-          <nav className="flex flex-col items-center border-b mb-5 px-5 py-3">
-            <div className="max-w-6xl w-full">
-              <MainNav />
-            </div>
-          </nav>
-          <main className="flex flex-col items-center">
-            <div className="max-w-6xl w-full">{children}</div>
-          </main>
-        </ThemeProvider>
+        <nav className="flex flex-col items-center border-b mb-5 px-5 py-3">
+          <div className="max-w-6xl w-full">
+            <MainNav />
+          </div>
+        </nav>
+        <main className="flex flex-col items-center">
+          <div className="max-w-6xl w-full">{children}</div>
+        </main>
       </body>
     </html>
   );
--- frontend/components/MainNav.tsx
+++ frontend/components/MainNav.tsx
@@ -1,11 +1,10 @@
 import React from "react";
 import MainNavLinks from "./MainNavLinks";
-import ToggleMode from "./ToggleMode";
 
 const MainNav = () => {
   return (
     <div className="flex justify-between">
-      <ToggleMode />
+      <div>Logo</div>
       <div className="flex items-center gap-4">
         <MainNavLinks />
       </div>
