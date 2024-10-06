3b8c21ad9908d47bcceae0cd0cf993c7047f0c53
Caleb Bae <baecal000@gmail.com>
2024-10-06 10:40:41 -0500

    LAST COMMIT x.x

--- frontend/components/BendDetection.tsx
+++ frontend/components/BendDetection.tsx
@@ -10,58 +10,58 @@ import Link from "next/link";
 import { Button } from "./ui/button";
 
 const BendDetection: React.FC = () => {
-    const { socket } = useContext(SocketContext);
-    const [bendCount, setBendCount] = useState(0);
+  const { socket } = useContext(SocketContext);
+  const [bendCount, setBendCount] = useState(0);
 
-    useEffect(() => {
-        if (socket) {
-            socket.on("feedback", (data: any) => {
-                console.log(data);
-                // You can also handle 'feedback' messages if needed
-                setBendCount(data.bend_count);
-            });
-        }
-
-        // Cleanup on component unmount
-        return () => {
-            if (socket) {
-                socket.off("feedback");
-            }
-        };
-    }, [socket]);
-
-    const handleFrameCaptured = (dataURL: string) => {
-        if (!socket) {
-            console.error("Socket not connected yet");
-            return;
-        }
-        console.log("Emitting video frame");
-        socket.emit("video_frame", dataURL);
-    };
-
-    if (!socket) {
-        return <div>Connecting to server...</div>; // Show a loading state while the socket is being initialized
+  useEffect(() => {
+    if (socket) {
+      socket.on("feedback", (data: any) => {
+        console.log(data);
+        // You can also handle 'feedback' messages if needed
+        setBendCount(data.bend_count);
+      });
     }
-    return (
-        <div>
-            <WebcamStream onFrameCaptured={handleFrameCaptured} />
-            <div>
-                <BendCounter count={bendCount} />
-            </div>
-            {bendCount >= 10 && (
-                <Link href="/excercise/workout">
-                    <div className="flex justify-center">
-                        <Button
-                            variant={"secondary"}
-                            className="rounded-2xl text-4xl px-5 py-4 mb-5"
-                        >
-                            Next
-                        </Button>
-                    </div>
-                </Link>
-            )}
-        </div>
-    );
+
+    // Cleanup on component unmount
+    return () => {
+      if (socket) {
+        socket.off("feedback");
+      }
+    };
+  }, [socket]);
+
+  const handleFrameCaptured = (dataURL: string) => {
+    if (!socket) {
+      console.error("Socket not connected yet");
+      return;
+    }
+    console.log("Emitting video frame");
+    socket.emit("video_frame", dataURL);
+  };
+
+  if (!socket) {
+    return <div>Connecting to server...</div>; // Show a loading state while the socket is being initialized
+  }
+  return (
+    <div>
+      <WebcamStream onFrameCaptured={handleFrameCaptured} />
+      <div>
+        <BendCounter count={bendCount} />
+      </div>
+      {bendCount >= 5 && (
+        <Link href="/excercise/workout">
+          <div className="flex justify-center">
+            <Button
+              variant={"secondary"}
+              className="rounded-2xl text-4xl px-5 py-4 mb-5"
+            >
+              Next
+            </Button>
+          </div>
+        </Link>
+      )}
+    </div>
+  );
 };
 
 export default BendDetection;
--- frontend/components/SquatDetection.tsx
+++ frontend/components/SquatDetection.tsx
@@ -9,56 +9,59 @@ import Link from "next/link";
 import { Button } from "./ui/button";
 
 const SquatDetection: React.FC = () => {
-    const { socket } = useContext(SocketContext);
-    const [squatCount, setSquatCount] = useState(0);
+  const { socket } = useContext(SocketContext);
+  const [squatCount, setSquatCount] = useState(0);
 
-    useEffect(() => {
-        if (socket) {
-            socket.on("feedback", (data: any) => {
-                console.log(data);
-                // You can also handle 'feedback' messages if needed
-                setSquatCount(data.count);
-            });
-        }
-
-        // Cleanup on component unmount
-        return () => {
-            if (socket) {
-                socket.off("feedback");
-            }
-        };
-    }, [socket]);
-
-    const handleFrameCaptured = (dataURL: string) => {
-        if (!socket) {
-            console.error("Socket not connected yet");
-            return;
-        }
-        console.log("Emitting video frame");
-        socket.emit("video_frame", dataURL);
-    };
-
-    if (!socket) {
-        return <div>Connecting to server...</div>; // Show a loading state while the socket is being initialized
+  useEffect(() => {
+    if (socket) {
+      socket.on("feedback", (data: any) => {
+        console.log(data);
+        // You can also handle 'feedback' messages if needed
+        setSquatCount(data.count);
+      });
     }
 
-    return (
-        <div className=" rounded-2xl">
-            <WebcamStream onFrameCaptured={handleFrameCaptured} />
-            <div>
-                <SquatCounter count={squatCount} />
-            </div>
-            {squatCount >= 10 && (
-                <Link href="/excercise">
-                    <div className="flex justify-center">
-                        <Button variant={"secondary"} className="rounded-2xl text-4xl px-5 py-4 mb-5">
-                            Finished
-                        </Button>
-                    </div>
-                </Link>
-            )}
-        </div>
-    );
+    // Cleanup on component unmount
+    return () => {
+      if (socket) {
+        socket.off("feedback");
+      }
+    };
+  }, [socket]);
+
+  const handleFrameCaptured = (dataURL: string) => {
+    if (!socket) {
+      console.error("Socket not connected yet");
+      return;
+    }
+    console.log("Emitting video frame");
+    socket.emit("video_frame", dataURL);
+  };
+
+  if (!socket) {
+    return <div>Connecting to server...</div>; // Show a loading state while the socket is being initialized
+  }
+
+  return (
+    <div className=" rounded-2xl">
+      <WebcamStream onFrameCaptured={handleFrameCaptured} />
+      <div>
+        <SquatCounter count={squatCount} />
+      </div>
+      {squatCount >= 5 && (
+        <Link href="/excercise">
+          <div className="flex justify-center">
+            <Button
+              variant={"secondary"}
+              className="rounded-2xl text-4xl px-5 py-4 mb-5"
+            >
+              Finished
+            </Button>
+          </div>
+        </Link>
+      )}
+    </div>
+  );
 };
 
 export default SquatDetection;
